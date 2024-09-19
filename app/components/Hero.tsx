b4193d8c00c2a9d923dd3be6ae04671bcd03f26e
kevocode22 <ocampokevin14@gmail.com>
2024-09-19 14:44:35 -0300

    UI: change text color

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -27,7 +27,7 @@ export default function Hero() {
   return (
     <AuroraBackground className="w-full flex justify-center items-center flex-col h-screen">
       <motion.div ref={scope}>
-        <h3 className="text-4xl text-nowrap sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-r from-emerald-400 via-teal-300 to-cyan-400 py-8 px-3">
+      <h3 className="text-4xl text-nowrap sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-r from-emerald-700 via-teal-600 to-cyan-700 py-8 px-3 dark:from-emerald-600 dark:via-teal-500 dark:to-cyan-600">
           Software Developer
         </h3>
       </motion.div>
